05affb2b8578f44350a09985db7febaf21fda335
Hibnu Hishath <hibnuhishath1@gmail.com>
2023-07-22 21:49:40 +0530

    Added support to automatically create the singleton assets

---
+++ Scripts/Attributes/AutoCreateScriptableSingletonAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hibzz.Singletons
+{
+    public class AutoCreateScriptableSingletonAttribute : Attribute { }
+}
